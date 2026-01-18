d5483afa22ce606ffbce15866141a3dfc9aea756
FlyingCow32 <gael.tournier32@gmail.com>
2026-01-18 22:19:54 +0100

    refactor(CSS Refactor)
    + Refactored CSS to make it less confusing
    + Added animations to tag sliders and settings

---
+++ script/newscript.js
@@ -0,0 +1,16 @@
+// TODO Add animation on the two tags container  and on settings page (add class active)
+// TODO Rafraischissement de toutes les valeurs au démarrage
+// TODO Créer l'objet de création des todos 
+// TODO Fonctionnalité pour valider les todos et tags avec entrée
+// TODO Faire en sorte que les bins fonctionnent 
+// TODO sauvegarde des données
+// TODO Style des todos quand elles sont barrées 
+// TODO fonction pour display les tags
+// TODO Fonction pour créer les tags 
+// TODO Fonction pour supprimer les tags 
+// TODO Actualisation de la checkbox avec le style au démarrage et quand elles sont checked
+// TODO Fonction pour actualiser les tags 
+// TODO Bouton pour tout reset 
+// TODO Fonctions de debug
+// TODO Changement de couleur dans les settings 
+// TODO Logique de tous les boutons 
